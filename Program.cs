81befa4c93e2ba7a327558a962f1fe69473b6eb4
exiw <51192517+exiw@users.noreply.github.com>
2021-09-21 20:03:30 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASCII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+
+            Console.WriteLine("En Büyük Türkiye");
+
+            Console.ReadKey();
+        }
+    }
+}
